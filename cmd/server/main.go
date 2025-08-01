34a171aeb7c1d22203206243f1afd3e2f8553d9f
LeMinh0706 <leminhken124356@gmail.com>
2025-08-01 15:37:51 +0700

    fix: run server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,29 +1,59 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"log"
 	"net"
+	"os"
+	"os/signal"
 
 	"github.com/LeMinh0706/todo/internal/todo"
 	"github.com/LeMinh0706/todo/proto"
+	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc"
 )
 
 func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	if err := run(ctx); err != nil && errors.Is(err, context.Canceled) {
+		log.Fatalf("failed to run server: %v", err)
+	}
+
+}
+
+func run(ctx context.Context) error {
 	grpcServer := grpc.NewServer()
 
 	list := &todo.List{}
 	service := todo.NewAddService(list)
 	proto.RegisterTodoServiceServer(grpcServer, service)
 
-	lis, err := net.Listen("tcp", ":50051")
-	if err != nil {
-		log.Fatal("failed to listen:", err)
-	}
+	g, ctx := errgroup.WithContext(ctx)
 
-	log.Println("gRPC server is running on port 50051")
+	g.Go(func() error {
 
-	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatal("failed to serve:", err)
-	}
+		lis, err := net.Listen("tcp", ":50051")
+		if err != nil {
+			return fmt.Errorf("failed to listen: %w", err)
+		}
+		log.Println("gRPC server is running on port 50051")
+
+		if err := grpcServer.Serve(lis); err != nil {
+			return fmt.Errorf("failed to serve gRPC server: %w", err)
+		}
+		return nil
+	})
+
+	g.Go(func() error {
+		<-ctx.Done()
+		log.Println("shutting down gRPC server")
+		grpcServer.GracefulStop()
+		return nil
+	})
+
+	return g.Wait()
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.24.4
 
 require (
 	github.com/google/uuid v1.6.0
+	golang.org/x/sync v0.14.0
 	google.golang.org/grpc v1.74.2
 	google.golang.org/protobuf v1.36.6
 )
